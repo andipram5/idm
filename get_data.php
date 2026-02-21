eb96949de131820ba6375b159efac0130d5c32fe
andipram5 <robbidarwin@gmail.com>
2026-02-21 08:46:40 +0700

    Update get_data.php

--- get_data.php
+++ get_data.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost","user_db","password_db","nama_db");
+$conn = new mysqli("localhost","root","phha8KKaFMraZOx7X4WYkJRJE6nlIrREM=XeAb9A5JTq","pos");
 
 if($conn->connect_error){
   die("Koneksi gagal");
@@ -7,5 +7,6 @@ if($conn->connect_error){
 
 $q = $conn->query("SELECT NOW() AS waktu");
 $r = $q->fetch_assoc();
+
 
 echo "Koneksi sukses : ".$r['waktu'];
